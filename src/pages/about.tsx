e8585a076b21ab908d44a79463dfa10a0d5af4ac
Wesley Williams <40679192+Moto42@users.noreply.github.com>
2023-06-22 13:59:44 -0500

    Links

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
+import { Link } from "gatsby";
 
 const AboutPage = () : React.Component => {
     return (
         <main>
             <h1>About me</h1>
+            <Link to='/'>Back to Home</Link>
             <p>Hi there! I'm the proud creator ofthis site, which I built with Gatsby</p>
         </main>
     );
