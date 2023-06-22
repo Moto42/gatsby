c8d2633afeca4703d901a9535bb107c9a963aeea
Wesley Williams <40679192+Moto42@users.noreply.github.com>
2023-06-22 15:24:23 -0500

    static images

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -12,14 +12,15 @@ const config: GatsbyConfig = {
   plugins: [
     "gatsby-plugin-sitemap",
     "gatsby-plugin-mdx",
-    {
-      resolve: 'gatsby-source-filesystem',
+    { resolve: 'gatsby-source-filesystem',
       options: {
         "name": "pages",
         "path": "./src/pages/"
       },
       __key: "pages"
-    }
+    },
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
   ]
 };
 
