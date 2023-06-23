b129226e9b97c5e8653b9c42761840573f683270
Wesley Williams <40679192+Moto42@users.noreply.github.com>
2023-06-22 21:48:57 -0500

    thinger

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,12 +1,25 @@
 import * as React from 'react';
 import Layout from '../components/layout';
 import Seo from '../components/seo';
-import { HeadFC } from 'gatsby';
+import { HeadFC, graphql } from 'gatsby';
 
-const BlogPage : React.FC = () => {
+type BlogPageProps = {
+    data: Queries.BlogPageQuery;
+};
+
+const BlogPage : React.FC<BlogPageProps> = ({data}) => {
+
     return (
         <Layout pageTitle="My Blog Posts">
-            <p>My cool posts will go in here.</p>
+            <ul>
+            {
+                data.allFile.edges.map(node => (
+                <li key={node.node.name}>
+                    {node.node.name}
+                </li>
+                ))
+            }
+            </ul>
         </Layout>
     );
 };
@@ -14,3 +27,15 @@ const BlogPage : React.FC = () => {
 export const Head: HeadFC = () => <Seo title="My Blog Posts"></Seo>;
 
 export default BlogPage;
+
+export const query = graphql`
+  query BlogPage {
+    allFile(filter: {sourceInstanceName: {eq: "blog"}}) {
+      edges {
+        node {
+          name
+        }
+      }
+    }
+  }
+`
